d171c0362cf08f191a52d762e5d9c6a799eb7a6d
Love2104 <Lovechourasia2004@gmail.com>
2024-05-10 00:15:26 +0530

    Profile

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function followed (){
+    var followEl = document.getElementById("follow")
+    followEl.textContent = "Unfollow";
+}
+followEl.addEventListener('click', function() {
+    var followEl = document.getElementById("follow");
+    alert('Button clicked!');
+  });
