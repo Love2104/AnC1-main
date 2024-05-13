de9c7e78a41261506452674112fa48709420c430
Love2104 <Lovechourasia2004@gmail.com>
2024-05-13 22:19:07 +0530

    Latest

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 
-function followed (){
-    var followEl = document.getElementById("follow")
-    followEl.textContent = "Unfollow";
-}
-followEl.addEventListener('click', function() {
-    var followEl = document.getElementById("follow");
-    alert('Button clicked!');
-  });
+
